2acc8e83768cd3bf3114302077a147f4a5889d60
David Godoy <dagodoy@ucm.es>
2021-10-06 21:33:59 +0200

    Empezado 2.1

--- P1/Regresion.py
+++ P1/Regresion.py
@@ -6,14 +6,16 @@ import matplotlib.pyplot as plt
 from pandas.io.parsers import read_csv
 from mpl_toolkits.mplot3d import Axes3D
 
+#-----------------------------------------------------------------------
 
 def carga_csv(file_name):
 
     valores = read_csv(file_name, header=None).to_numpy()
     return valores.astype(float)
 
+#-----------------------------------------------------------------------
 
-def regresion():
+def regresion_unica():
     datos = carga_csv('ex1data1.csv')
     X = datos[:, 0]
     Y = datos[:, 1]
@@ -27,14 +29,17 @@ def regresion():
             sum_1 += ((theta_0 + theta_1 * X[i]) - Y[i]) * X[i]
         theta_0 = theta_0 - (alpha / m) * sum_0
         theta_1 = theta_1 - (alpha / m) * sum_1
-    #plt.plot(X, Y, "x")
+    plt.plot(X, Y, "x")
     min_x = min(X)
     max_x = max(X)
     min_y = theta_0 + theta_1 * min_x
     max_y = theta_0 + theta_1 * max_x
-    #plt.plot([min_x, max_x], [min_y, max_y])
-    #plt.savefig("resultado.pdf")
+    plt.plot([min_x, max_x], [min_y, max_y])
+    plt.savefig("resultado.pdf")
+    plt.clf()
     return [theta_0, theta_1]
+
+#-----------------------------------------------------------------------
 
 def coste(X,Y,theta):
     aux = 0
@@ -42,6 +47,8 @@ def coste(X,Y,theta):
     for i in range(m):
         aux += ((theta[0] + theta[1] * X[i])-Y[i])**2
     return aux/(2*m)
+
+#-----------------------------------------------------------------------
 
 def make_data(t0_range, t1_range, X, Y):
 
@@ -54,28 +61,77 @@ def make_data(t0_range, t1_range, X, Y):
     for ix, iy in np.ndindex(Theta0.shape):
         Coste[ix, iy] = coste(X, Y, [Theta0[ix, iy], Theta1[ix, iy]])
 
-  
-
     return [Theta0, Theta1, Coste]
 
-datos = carga_csv('ex1data1.csv')
-X = datos[:, 0]
-Y = datos[:, 1]
-t0=[-10,10]
-t1=[-1,4]
+#-----------------------------------------------------------------------
 
-puntito=regresion()
-c = make_data(t0,t1,X,Y)
+def dibuja_contorno(puntito, c):
+    plt.plot(puntito[0],puntito[1], "x", color='red')
+    plt.contour(c[0],c[1],c[2], np.logspace(-2, 3, 20), cmap='rainbow')
 
-plt.plot(puntito[0],puntito[1], "x", color='red')
-plt.contour(c[0],c[1],c[2], np.logspace(-2, 3, 20), cmap='rainbow')
-plt.savefig("contorno.png") 
+    plt.savefig("contorno.png") 
+    plt.clf()
 
-plt.clf()
+#-----------------------------------------------------------------------
 
-fig = plt.figure()
-ax = Axes3D(fig)
+def dibuja_superficie(c):
+    fig = plt.figure()
+    ax = Axes3D(fig)
+    ax.plot_surface(c[0],c[1],c[2], cmap='rainbow',linewidth=0, antialiased=False)
 
-surf = ax.plot_surface(c[0],c[1],c[2], cmap='rainbow',linewidth=0, antialiased=False)
+    plt.savefig("superficie.png")   
+    plt.clf()
 
-plt.savefig("superficie.png") 
+#-----------------------------------------------------------------------
+
+def graficas1():
+    datos = carga_csv('ex1data1.csv')
+    X = datos[:, 0]
+    Y = datos[:, 1]
+
+    puntito=regresion_unica()
+    c = make_data([-10,10],[-1,4],X,Y)
+
+    dibuja_contorno(puntito, c)
+    dibuja_superficie(c)
+
+#---------------------------------------------------------------------------------------------------------------------------------
+
+def regresion_varias():
+    datos = carga_csv('ex1data2.csv')
+
+    X = datos[:, :-1] 
+    Y = datos[:, -1]
+
+    X_norm, x_mu, x_sigma = normalizar_mat(X)
+    Y_norm, y_mu, y_simga = normalizar_mat(Y)
+
+    m = np.shape(X)[0]
+    n = np.shape(X)[1]
+
+    # añadimos una columna de 1's a la X
+    X_norm = np.hstack([np.ones([m, 1]), X])
+
+    theta = np.full(n+1, 0)
+    alpha = 0.01
+    #Thetas, costes = descenso_gradiente(X, Y, alpha)
+
+#-----------------------------------------------------------------------
+
+def coste(X, Y, Theta):
+    H = np.dot(X, Theta)
+    Aux = (H - Y) ** 2
+    return Aux.sum() / (2 * len(X))
+
+#-----------------------------------------------------------------------
+
+def normalizar_mat(X):
+    mu = np.mean(X, 0)
+    sigma = np.std(X, 0)
+    X_norm = (X-mu)/sigma
+    return X_norm, mu, sigma
+
+#---------------------------------------------------------------------------------------------------------------------------------
+
+#graficas1()
+regresion_varias()
